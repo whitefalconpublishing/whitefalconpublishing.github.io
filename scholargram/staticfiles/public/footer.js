edc75e3122c963b866ebb925ed532e598314cae9
whitefalconpublishing <whitefalconpublishing@gmail.com>
2015-11-21 15:55:32 +0530

    updated scholargram static files

---
+++ scholargram/staticfiles/public/footer.js
@@ -0,0 +1,67 @@
+document.write('            <footer id="footer" class="content">');
+document.write('                <div class="row">');
+document.write('                    <div class="1u">');
+document.write('                                <section>');
+document.write('                                    <h2>&nbsp;</h2>');
+document.write('                                </section>');
+document.write('                            </div>');
+document.write('                    <div class="2u">');
+document.write('                                <section>');
+document.write('                                    <h2>Links</h2>');
+document.write('                                    <ul class="alt1">');
+document.write('                                        <li><a href="/">Home</a></li>');
+document.write('                                        <li><a href="/catalog/">Books Published</a></li>');
+document.write('                                        <li><a href="/process/">Process</a></li>');
+document.write('                                        <li><a href="/services/">Services</a></li>');
+document.write('                                        <li><a href="/distribution/">Distribution</a></li>');
+document.write('                                        <li><a href="/faq/">FAQs</a></li>');
+document.write('                                        <li><a href="/dashboard/">Dashboard</a></li>');
+document.write('                                        <li><a href="/contact/">Contact</a></li>');
+document.write('                                    </ul>');
+document.write('                                </section>');
+document.write('                            </div>');
+
+document.write('                    <div class="3u">');
+document.write('                        <section>');
+document.write('                            <h2>Get in touch</h2>');
+document.write('                            <p>Email:<br>');
+document.write('                            contact@scholargram.com<br>');
+document.write('                        </section>');
+document.write('                    </div>');
+document.write('                            <div class="2u">');
+document.write('                                <section>');
+document.write('                                    <h2>Follow us</h2>');
+
+document.write('<a href="http://facebook.com/whitefalconpublishing" target="_blank" ><img src="/static/public/images/fb.jpg" ></a><br>');
+
+document.write('<a href="https://twitter.com/WhiteFalconpub" target="_blank"><img src="/static/public/images/twitter.jpg" ></a><br>');
+
+document.write('<a href="https://plus.google.com/116649724837582317540/posts" target="_blank"><img src="/static/public/images/gplus.jpg" ></a><br>');
+  
+document.write('                                </section>');
+document.write('                            </div>');
+document.write('                    <div class="2u">');
+document.write('                        <!-- Blurb -->');
+document.write('                        <section>');
+document.write('                            <a href="http://www.self-publish.in" class="image image-full"><img width="100%" src="/static/public/images/sglogo.png" alt=""></a>');
+document.write('                            <p>Powered by: <br><a href="http://www.self-publish.in">White Falcon Publishing Solutions LLP</a><br/> ');
+document.write('                                Registered Address:<br/>');
+document.write('                                No. 335, Sector 48A<br/>');
+document.write('                                Chandigarh - 160047, INDIA');
+document.write('                            </p>');
+document.write('                        </section>');
+document.write('                    </div>');
+document.write('                </div>');
+
+document.write('        <!-- Copyright -->');
+document.write('                <div class="row">');
+document.write('                    <div class="12u">');
+document.write('        <center>');
+document.write('        <div class="copyright">');
+document.write('           <p> ScholarGram powered by <a href="http://www.self-publish.in">White Falcon Publishing Solutions LLP</a><br>');
+document.write('            <a href="/terms/">Terms and Conditions</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="/privacy/">Privacy Policy</a><br>');
+document.write('            &copy; 2015 <a href="http://www.self-publish.in">White Falcon Publishing Solutions LLP</a>. All rights reserved </p>');
+document.write('        </center>');
+document.write('        </div>');
+document.write('        </div>');
+document.write('            </footer>');
---
+++ scholargram/staticfiles/public/header.js
@@ -0,0 +1,31 @@
+var buttonlabel = document.getElementById("buttonlabel").value;
+
+document.write('<header id="header" class="alt">');
+document.write('				<h1><a href="/"><img src="/static/public/images/sglogo.png" height="95%"></a></h1>');
+document.write('				<div id="ctaa">');
+document.write('				<form class="form-horizontal" action="/search/" method="post" role="form" >');
+document.write('							<input type="text" name="query" id="query" placeholder="Search" />');
+document.write('				</form>');
+document.write('			</div>');				
+document.write('				<nav id="nav">');
+document.write('					<ul>');
+document.write('						<li><a href="/">Home</a></li>');
+document.write('						<li><a href="/catalog/">Books</a></li>');
+document.write('						<li><a href="/process/">Process</a></li>');
+document.write('								<li><a href="/distribution/">Distribution</a></li>');	
+document.write('<li>');
+document.write('<a href="">Resources</a>');
+document.write('<ul>');
+document.write('						<li><a href="/services/">Services</a></li>');	
+document.write('								<li><a href="/faq/">FAQ</a></li>');
+document.write('</ul>');
+document.write('</li>');
+					
+								
+
+
+document.write('						<li><a href="/contact/">Contact</a></li>');
+document.write('						<li><a href="/dashboard/" class="button">'+buttonlabel+'</a></li>');
+document.write('					</ul>');
+document.write('				</nav>');		
+document.write('			</header>');
