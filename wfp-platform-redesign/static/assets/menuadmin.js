5b1b2ae4ed06b9fab1be631e07a078ecaccfee80
Unknown <arshdeepbahga@gmail.com>
2019-04-28 19:45:51 +0530

    updated admin menu lcons

--- wfp-platform-redesign/static/assets/menuadmin.js
+++ wfp-platform-redesign/static/assets/menuadmin.js
@@ -13,14 +13,14 @@ document.write("								<li id=\"income-menu\"><a href=\"\/income-report\"><i cl
 document.write("								<li id=\"expense-menu\"><a href=\"\/expense-report\"><i class=\"icon-cash2\"><\/i> <span>Expense Report Generator<\/span><\/a><\/li>");
 document.write("								<li id=\"royalty-update-checker-menu\"><a href=\"\/royalty-update-checker\"><i class=\"icon-download\"><\/i> <span>Royalty Update Checker<\/span><\/a><\/li>");
 document.write("								<li id=\"royalty-updater-menu\"><a href=\"\/royalty-updater\"><i class=\"icon-database-upload\"><\/i> <span>Royalty Updater Tool<\/span><\/a><\/li>");
-document.write("								<li id=\"return-update-checker-menu\"><a href=\"\/return-update-checker\"><i class=\"icon-download\"><\/i> <span>Return Update Checker<\/span><\/a><\/li>");
-document.write("								<li id=\"return-updater-menu\"><a href=\"\/return-updater\"><i class=\"icon-database-upload\"><\/i> <span>Return Updater Tool<\/span><\/a><\/li>");
+document.write("								<li id=\"return-update-checker-menu\"><a href=\"\/return-update-checker\"><i class=\"icon-download7\"><\/i> <span>Return Update Checker<\/span><\/a><\/li>");
+document.write("								<li id=\"return-updater-menu\"><a href=\"\/return-updater\"><i class=\"icon-upload7\"><\/i> <span>Return Updater Tool<\/span><\/a><\/li>");
 document.write("                                <li id=\"order-mailer-checker-menu\"><a href=\"\/order-mailer-checker\"><i class=\"icon-stack-down\"><\/i> <span>Weekly Order Mailer Checker<\/span><\/a><\/li>");
 document.write("                                <li id=\"order-mailer-menu\"><a href=\"\/order-mailer\"><i class=\"icon-mail5\"><\/i> <span>Weekly Order Mailer Tool<\/span><\/a><\/li>");
-document.write("								<li id=\"isbngenerator-menu\"><a href=\"\/isbn-generator\"><i class=\"icon-book\"><\/i> <span>ISBN Generator<\/span><\/a><\/li>");
+document.write("								<li id=\"isbngenerator-menu\"><a href=\"\/isbn-generator\"><i class=\"icon-barcode2\"><\/i> <span>ISBN Generator<\/span><\/a><\/li>");
 document.write("								<li id=\"covertemplate-menu\"><a href=\"\/cover-template\"><i class=\"icon-book\"><\/i> <span>Cover Template Generator<\/span><\/a><\/li>");
-document.write("								<li id=\"pdffixer-menu\"><a href=\"\/pdf-fixer\"><i class=\"icon-book\"><\/i> <span>PDF Fixer for Duplex Print<\/span><\/a><\/li>");
-document.write("                                <li id=\"blockchain-dapp\"><a href=\"\/blockchain-dapp\"><i class=\"icon-download\"><\/i> <span>Blockchain Certify Dapp<\/span><\/a><\/li>");
+document.write("								<li id=\"pdffixer-menu\"><a href=\"\/pdf-fixer\"><i class=\"icon-file-pdf\"><\/i> <span>PDF Fixer for Duplex Print<\/span><\/a><\/li>");
+document.write("                                <li id=\"blockchain-dapp\"><a href=\"\/blockchain-dapp\"><i class=\"icon-link2\"><\/i> <span>Blockchain Certify Dapp<\/span><\/a><\/li>");
 
 document.write("									<\/ul>");
 document.write("								<\/li>");
